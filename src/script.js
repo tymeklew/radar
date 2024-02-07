562a23d769e71fa88d4a7b4ed421908201a79650
Tymek Lewandowski <tymek@tymeks-air.lan>
2024-02-07 16:22:53 +0000

    started the actual form

--- src/script.js
+++ src/script.js
@@ -1,12 +1,47 @@
 const graphElement = document.querySelector("#graph");
+const tableEleent = document.querySelector("#data-table");
 
 var creator = new Creator(800, 800);
-creator.label_size = 18;
 creator.setTargetElement(graphElement);
 
-creator.addLabel("Geography");
-creator.addLabel("Physics");
-creator.addLabel("Physics");
-creator.addLabel("Physics");
-creator.addLabel("Physics");
-creator.addData([5, 10, 8, 2, 4]);
+var state = {
+  labels: ["English", "Physics", "Biology"],
+  variants: [[1, 2, 3]],
+};
+
+function renderVariants() {
+  state.variants.forEach((data, var_index) => {
+    const parent = document.createElement("tr");
+    const name = document.createElement("td");
+
+    name.textContent = "Variant";
+    parent.appendChild(name);
+
+    data.forEach((value, data_index) => {
+      const child = document.createElement("td");
+      const input = document.createElement("input");
+
+      input.type = "number";
+      input.onchange = (ev) => {
+        creator.data[var_index][data_index] = parseInt(ev.target.value);
+        creator.updateMaxDataValue();
+        updateGraph();
+      };
+      input.value = value;
+
+      child.appendChild(input);
+      parent.appendChild(child);
+    });
+    tableEleent.appendChild(parent);
+  });
+}
+
+function updateGraph() {
+  creator.labels = state.labels;
+  creator.data = state.variants;
+  console.log(creator);
+  creator.render();
+}
+
+renderVariants();
+updateGraph();
