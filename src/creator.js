f32d87c9dc1a7e9d5c3977038baf17a14cbd4fa6
Tymek Lewandowski <tymek@tymeks-air.lan>
2024-02-14 20:30:31 +0000

    Some misc stuff

--- src/creator.js
+++ src/creator.js
@@ -2,211 +2,235 @@ const ns = "http://www.w3.org/2000/svg";
 const COMPLETE_ANGLE = 2 * Math.PI;
 
 class Creator {
-  constructor(width, height) {
-    // Initate the svg
-    this.svg = document.createElementNS(ns, "svg");
-    this.svg.setAttribute("width", width);
-    this.svg.setAttribute("height", height);
+	constructor(width, height) {
+		// Initate the svg
+		this.svg = document.createElementNS(ns, "svg");
+		this.svg.setAttribute("width", width);
+		this.svg.setAttribute("height", height);
 
-    // Size of the text element in the graph
-    this.label_size = 5;
-    this.width = width;
-    this.height = height;
-    // Where the creator will render the graph on the DOM
-    this.targetElement = null;
+		// Size of the text element in the graph
+		this.label_size = 30;
+		this.width = width;
+		this.height = height;
+		// Where the creator will render the graph on the DOM
+		this.targetElement = null;
 
-    // Array of labels for the graph
-    // E.G ["Geography", "Physics"]
-    this.labels = [];
-    /// Array of data points must match the length of the labels
-    // E.G [[5 , 10] , [7 , 9]]
-    this.data = [];
-  }
+		// Settings for the scale lines
+		this.labelLines = 10;
 
-  // Sets the target element to render the graph to
-  setTargetElement(element) {
-    this.targetElement = element;
-  }
+		// Array of labels for the graph
+		// E.G ["Geography", "Physics"]
+		this.labels = [];
+		/// Array of data points must match the length of the labels
+		// E.G [[5 , 10] , [7 , 9]]
+		this.data = [];
+	}
 
-  // Retunrs origional position of the graph
-  originPos() {
-    return {
-      x: this.width / 2,
-      y: this.height / 2,
-    };
-  }
+	setTargetElement(element) {
+		this.targetElement = element;
+	}
 
-  // Returns dimensions of the graph
-  dimensions() {
-    return {
-      width: (this.width / 2) * 0.8,
-      height: (this.height / 2) * 0.8,
-    };
-  }
+	// Retunrs origional position of the graph
+	originPos() {
+		return {
+			x: this.width / 2,
+			y: this.height / 2,
+		};
+	}
 
-  // Add a label to the graph
-  addLabel(label) {
-    this.labels.push(label);
-    this.render();
-  }
+	// Returns dimensions of the graph
+	dimensions() {
+		return {
+			width: (this.width / 2) * 0.8,
+			height: (this.height / 2) * 0.8,
+		};
+	}
 
-  /// Add data to the graph
-  addData(data) {
-    //Sanatize the data and ensure it has the right amount of data points
-    this.data.push(data);
-    this.updateMaxDataValue();
-    this.render();
-  }
+	// Add a label to the graph
+	addLabel(label) {
+		this.labels.push(label);
+		this.render();
+	}
 
-  // Update the max data value
-  updateMaxDataValue() {
-    this.maxValue = Math.max(...this.data.map((data) => Math.max(...data)));
-  }
+	/// Add data to the graph
+	addData(data) {
+		//Sanatize the data and ensure it has the right amount of data points
+		this.data.push(data);
+		this.updateMaxDataValue();
+		this.render();
+	}
 
-  render() {
-    this.svg.innerHTML = "";
-    // Dont render if there is no data
-    if (this.data.length == 0 || this.labels.length == 0) {
-      return;
-    }
+	// Update the max data value
+	updateMaxDataValue() {
+		this.maxValue = Math.max(...this.data.map((data) => Math.max(...data)));
+	}
 
-    // Angle gap between each point theta in rad
-    let angle = COMPLETE_ANGLE / this.labels.length;
+	render() {
+		this.svg.innerHTML = "";
+		// Dont render if there is no data
+		if (this.data.length == 0 || this.labels.length == 0) {
+			return;
+		}
 
-    const dimensions = this.dimensions();
-    const origin = this.originPos();
+		// Angle gap between each point theta in rad
+		let angle = COMPLETE_ANGLE / this.labels.length;
 
-    // Add each of the labels onto the svg
-    this.labels.forEach((text, index) => {
-      let label = document.createElementNS(ns, "text");
+		const dimensions = this.dimensions();
+		const origin = this.originPos();
 
-      const x =
-        origin.x +
-        dimensions.width * Math.cos(angle * index - COMPLETE_ANGLE / 4);
-      const y =
-        origin.y +
-        dimensions.height * Math.sin(angle * index - COMPLETE_ANGLE / 4);
+		// Add each of the labels onto the svg
+		this.labels.forEach((text, index) => {
+			let label = document.createElementNS(ns, "text");
 
-      label.setAttribute("x", x);
-      label.setAttribute("y", y);
-      label.setAttribute("font-size", this.label_size);
+			const x =
+				origin.x +
+				dimensions.width * Math.cos(angle * index - COMPLETE_ANGLE / 4);
+			const y =
+				origin.y +
+				dimensions.height * Math.sin(angle * index - COMPLETE_ANGLE / 4);
 
-      label.textContent = text;
+			label.setAttribute("x", x);
+			label.setAttribute("y", y);
+			label.setAttribute("font-size", this.label_size);
 
-      this.svg.appendChild(label);
-    });
+			label.textContent = text;
 
-    // Add the data to the graph
-    this.data.forEach((data) => {
-      this.svg.appendChild(this.renderPolygon(data));
-    });
+			this.svg.appendChild(label);
+		});
 
-    this.svg.appendChild(this.renderScale());
+		// Add the data to the graph
+		this.data.forEach((data) => {
+			this.svg.appendChild(this.renderPolygon(data));
+		});
 
-    this.targetElement.innerHTML = this.svg.outerHTML;
-  }
+		this.svg.appendChild(this.renderScale());
 
-  // Render a scale onto the graph
-  renderScale() {
-    let grouping = document.createElementNS(ns, "g");
+		this.targetElement.innerHTML = this.svg.outerHTML;
+	}
 
-    let angle = COMPLETE_ANGLE / this.labels.length;
-    const origin = this.originPos();
-    const dimensions = this.dimensions();
+	// Render a scale onto the graph
+	renderScale() {
+		let grouping = document.createElementNS(ns, "g");
 
-    // Render the vertical lines directly to the labels
-    this.labels.forEach((_, index) => {
-      const line = document.createElementNS(ns, "line");
+		let angle = COMPLETE_ANGLE / this.labels.length;
+		const origin = this.originPos();
+		const dimensions = this.dimensions();
 
-      line.setAttribute("stroke", "#000000");
-      line.setAttribute("opacity", 0.4);
-      line.setAttribute("x1", origin.x);
-      line.setAttribute("y1", origin.y);
+		// Render the vertical lines directly to the labels
+		this.labels.forEach((_, index) => {
+			const line = document.createElementNS(ns, "line");
 
-      const x =
-        origin.x +
-        dimensions.width * Math.cos(angle * index - COMPLETE_ANGLE / 4);
-      const y =
-        origin.x +
-        dimensions.height * Math.sin(angle * index - COMPLETE_ANGLE / 4);
+			line.setAttribute("stroke", "#000000");
+			line.setAttribute("opacity", 0.4);
+			line.setAttribute("x1", origin.x);
+			line.setAttribute("y1", origin.y);
 
-      line.setAttribute("x2", x);
-      line.setAttribute("y2", y);
+			const x =
+				origin.x +
+				dimensions.width * Math.cos(angle * index - COMPLETE_ANGLE / 4);
+			const y =
+				origin.x +
+				dimensions.height * Math.sin(angle * index - COMPLETE_ANGLE / 4);
 
-      grouping.appendChild(line);
-    });
+			line.setAttribute("x2", x);
+			line.setAttribute("y2", y);
 
-    // Render the polygons onto the graph for the data points scaling
-    for (let i = 0; i < 11; i++) {
-      const polygon = document.createElementNS(ns, "polygon");
-      polygon.setAttribute("stroke", "#000000");
-      polygon.setAttribute("opacity", 0.4);
-      polygon.setAttribute("fill", "none");
+			grouping.appendChild(line);
+		});
 
-      let points = "";
+		// Render the polygons onto the graph for the data points scaling
+		for (let i = 0; i <= this.labelLines; i++) {
+			const polygon = document.createElementNS(ns, "polygon");
+			polygon.setAttribute("stroke", "#000000");
+			polygon.setAttribute("opacity", 0.4);
+			polygon.setAttribute("fill", "none");
 
-      this.labels.forEach((_, index) => {
-        let width = (i / 10) * dimensions.width;
-        let height = (i / 10) * dimensions.height;
+			let points = "";
 
-        const x =
-          origin.x + width * Math.cos(angle * index - COMPLETE_ANGLE / 4);
-        const y =
-          origin.y + height * Math.sin(angle * index - COMPLETE_ANGLE / 4);
-        points += `${x},${y} `;
-      });
-      polygon.setAttribute("points", points);
+			this.labels.forEach((_, index) => {
+				let scale = (i / (this.labelLines));
 
-      grouping.appendChild(polygon);
-    }
+				let width = scale * dimensions.width;
+				let height = scale * dimensions.height;
 
-    return grouping;
-  }
+				const x =
+					origin.x + width * Math.cos(angle * index - COMPLETE_ANGLE / 4);
+				const y =
+					origin.y + height * Math.sin(angle * index - COMPLETE_ANGLE / 4);
+				points += `${x},${y} `;
+			});
+			polygon.setAttribute("points", points);
 
-  // Render the polygon onto the graph for the data points
-  renderPolygon(data) {
-    let polygon = document.createElementNS(ns, "polygon");
+			grouping.appendChild(polygon);
+		}
 
-    polygon.setAttribute("fill", "#ff0000");
-    polygon.setAttribute("fill-opacity", 0.2);
-    polygon.setAttribute("stroke", "#ff0000");
+		return grouping;
+	}
 
-    const angle = COMPLETE_ANGLE / this.labels.length;
-    const origin = this.originPos();
-    const dimensions = this.dimensions();
+	// Render the polygon onto the graph for the data points
+	renderPolygon(data) {
+		let grouping = document.createElementNS(ns, "g");
+		let polygon = document.createElementNS(ns, "polygon");
 
-    let points = "";
-    for (let i = 0; i < this.labels.length; i++) {
-      const x =
-        origin.x +
-        (data[i] / this.maxValue) *
-          dimensions.width *
-          Math.cos(angle * i - COMPLETE_ANGLE / 4);
-      const y =
-        origin.y +
-        (data[i] / this.maxValue) *
-          dimensions.height *
-          Math.sin(angle * i - COMPLETE_ANGLE / 4);
+		polygon.setAttribute("fill", "#ff0000");
+		polygon.setAttribute("fill-opacity", 0.2);
+		polygon.setAttribute("stroke", "#ff0000");
 
-      points += `${x},${y} `;
-    }
+		const angle = COMPLETE_ANGLE / this.labels.length;
+		const origin = this.originPos();
+		const dimensions = this.dimensions();
 
-    polygon.setAttribute("points", points);
+		let points = "";
+		for (let i = 0; i < this.labels.length; i++) {
+			const x =
+				origin.x +
+				(data[i] / this.maxValue) *
+				dimensions.width *
+				Math.cos(angle * i - COMPLETE_ANGLE / 4);
+			const y =
+				origin.y +
+				(data[i] / this.maxValue) *
+				dimensions.height *
+				Math.sin(angle * i - COMPLETE_ANGLE / 4);
 
-    return polygon;
-  }
 
-  label(label, x, y) {
-    const text = document.createElementNS(ns, "text");
-    text.setAttribute("x", x);
-    text.setAttribute("y", y);
-    text.setAttribute("font-size", this.label_size);
+			let point = document.createElementNS(ns, "circle");
+			point.setAttribute("cx", x);
+			point.setAttribute("cy", y);
+			point.setAttribute("r", 5);
+			point.setAttribute("fill", "#ff0000");
+			point.setAttribute("fill-opacity", 1);
 
-    text.textContent = label;
-    return text;
-  }
+			grouping.appendChild(point);
 
-  ToString() {
-    return this.svg.outerHTML;
-  }
+			points += `${x},${y} `;
+		}
+
+		polygon.setAttribute("points", points);
+		grouping.appendChild(polygon);
+
+		return grouping;
+	}
+
+	label(label, x, y) {
+		const text = document.createElementNS(ns, "text");
+		text.setAttribute("x", x);
+		text.setAttribute("y", y);
+		text.setAttribute("font-size", this.label_size);
+
+		text.textContent = label;
+		return text;
+	}
+
+	// Gets the download link this is what is used to download the file 
+	downloadLink() {
+		let serializer = new XMLSerializer();
+		let source = serializer.serializeToString(this.svg);
+
+		return "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
+	}
+
+	ToString() {
+		return this.svg.outerHTML;
+	}
 }
--- src/script.js
+++ src/script.js
@@ -5,46 +5,46 @@ var creator = new Creator(800, 800);
 creator.setTargetElement(graphElement);
 
 var state = {
-  labels: ["English", "Physics", "Biology", "Chemistry", "Geology"],
-  variants: [[1, 2, 3, 4, 5]],
+	labels: ["English", "Physics", "Biology", "Chemistry", "Geology"],
+	variants: [[1, 2, 3, 4, 5]],
 };
 
 function renderVariants() {
-  state.variants.forEach((data, var_index) => {
-    const parent = document.createElement("tr");
-    const name = document.createElement("td");
+	state.variants.forEach((data, var_index) => {
+		const parent = document.createElement("tr");
+		const name = document.createElement("td");
 
-    name.textContent = "Variant";
-    parent.appendChild(name);
+		name.textContent = "Variant";
+		parent.appendChild(name);
 
-    data.forEach((value, data_index) => {
-      const child = document.createElement("td");
-      const input = document.createElement("input");
+		data.forEach((value, data_index) => {
+			const child = document.createElement("td");
+			const input = document.createElement("input");
 
-      input.type = "number";
-      input.onchange = (ev) => {
-        creator.data[var_index][data_index] = parseInt(ev.target.value);
-        creator.updateMaxDataValue();
-        updateGraph();
-      };
-      input.value = value;
+			input.type = "number";
+			input.onchange = (ev) => {
+				creator.data[var_index][data_index] = parseInt(ev.target.value);
+				creator.updateMaxDataValue();
+				updateGraph();
+			};
+			input.value = value;
 
-      child.appendChild(input);
-      parent.appendChild(child);
-    });
-    tableEleent.appendChild(parent);
-  });
+			child.appendChild(input);
+			parent.appendChild(child);
+		});
+		tableEleent.appendChild(parent);
+	});
 }
 
 function updateGraph() {
-  creator.labels = state.labels;
-  creator.data = state.variants;
-  creator.updateMaxDataValue();
-  creator.render();
+	creator.labels = state.labels;
+	creator.data = state.variants;
+	creator.updateMaxDataValue();
+	creator.render();
 }
 
 window.onload = () => {
-  console.log(creator);
-  renderVariants();
-  updateGraph();
+	console.log(creator);
+	renderVariants();
+	updateGraph();
 };
